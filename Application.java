4ee4b45afb7c9d12751dad7ce7ff16dd3e186fb9
SPMaridevaramath <spmaridevaramath.xworkz@gmail.com>
2024-06-24 12:58:07 +0530

    updated

---
+++ Application.java
@@ -0,0 +1,92 @@
+public class Application{
+//Declare 9 static varibles 
+
+   static String applicationName="Whatsapp";
+   static String  createdBy="Brain Acton and Jan kaum";
+   static float  ratingOutof5=4.5f;
+   static long   usersOfwhatsapp=3026000000l;
+   static float version=2.2f;
+   static int maxGroupsizeinmb=256;
+   static int fileSizeinmb=100;
+   static int defaultStatusdisplaiesdurationinhours=24;
+   static int statusDurationinseconds=60;
+   
+
+
+   public static void main(String args[]){
+     //Declare 8 local varibles
+	 
+	    String userName = "Siddu";
+        String userPhoneNumber = "+1234567890";
+        boolean notificationsEnabled = true;
+        String chatBackground = "default_background.png";
+        boolean readReceiptsEnabled = true;
+        boolean typingIndicatorsEnabled = true;
+        int fontSize = 14;
+        String language = "English";
+		
+		
+		//Print 9 static varibles
+		
+		System.out.println("applicationName:"+applicationName);
+		System.out.println("createdBy:"+createdBy);
+		System.out.println("ratingOutof5:"+ratingOutof5);
+		System.out.println("usersOfwhatsapp:"+usersOfwhatsapp);
+		System.out.println("version:"+version);
+		System.out.println("maxGroupsizeinmb:"+maxGroupsizeinmb);
+		System.out.println("fileSizeinmb:"+fileSizeinmb);
+		System.out.println("defaultStatusdisplaiesdurationinhours:"+defaultStatusdisplaiesdurationinhours);
+		System.out.println("statusDurationinseconds:"+statusDurationinseconds);
+		
+		
+		
+		
+		//Print 8 local varibles
+		System.out.println("userName:"+userName);
+		System.out.println("userPhoneNumber:"+userPhoneNumber);
+		System.out.println("notificationsEnabled:"+notificationsEnabled);
+		System.out.println("chatBackground:"+chatBackground);
+		System.out.println("readReceiptsEnabled:"+readReceiptsEnabled);
+		System.out.println("typingIndicatorsEnabled:"+typingIndicatorsEnabled);
+		System.out.println("fontSize:"+fontSize);
+		System.out.println("language:"+language);
+		
+		
+   }
+}   
+		
+		
+		
+		
+		
+		
+		
+
+	 
+	 
+	 
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
+
+
+
+
+
+
+
